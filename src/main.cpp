4da40a092b03864d8ffdae2f7a0ec7cdd76c2b94
Chris Ohk <utilForever@gmail.com>
2020-04-20 23:02:09 +0900

    feat(chap-13): Where Next?

--- src/main.cpp
+++ src/main.cpp
@@ -45,35 +45,78 @@ vec3 ray_color(const ray& r, const hittable& world, int depth)
     return (1.0 - t) * vec3{1.0, 1.0, 1.0} + t * vec3{0.5, 0.7, 1.0};
 }
 
+hittable_list random_scene()
+{
+    hittable_list world;
+
+    world.add(std::make_shared<sphere>(
+        vec3{0, -1000, 0}, 1000,
+        std::make_shared<lambertian>(vec3{0.5, 0.5, 0.5})));
+
+    for (int a = -11; a < 11; ++a)
+    {
+        for (int b = -11; b < 11; ++b)
+        {
+            const auto choose_mat = random_double();
+            vec3 center{a + 0.9 * random_double(), 0.2,
+                        b + 0.9 * random_double()};
+            if ((center - vec3{4, 0.2, 0}).length() > 0.9)
+            {
+                if (choose_mat < 0.8)
+                {
+                    // diffuse
+                    auto albedo = vec3::random() * vec3::random();
+                    world.add(std::make_shared<sphere>(
+                        center, 0.2, std::make_shared<lambertian>(albedo)));
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // metal
+                    auto albedo = vec3::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    world.add(std::make_shared<sphere>(
+                        center, 0.2, std::make_shared<metal>(albedo, fuzz)));
+                }
+                else
+                {
+                    // glass
+                    world.add(std::make_shared<sphere>(
+                        center, 0.2, std::make_shared<dielectric>(1.5)));
+                }
+            }
+        }
+    }
+
+    world.add(std::make_shared<sphere>(vec3{0, 1, 0}, 1.0,
+                                       std::make_shared<dielectric>(1.5)));
+
+    world.add(std::make_shared<sphere>(
+        vec3{-4, 1, 0}, 1.0,
+        std::make_shared<lambertian>(vec3(0.4, 0.2, 0.1))));
+
+    world.add(std::make_shared<sphere>(
+        vec3{4, 1, 0}, 1.0, std::make_shared<metal>(vec3(0.7, 0.6, 0.5), 0.0)));
+
+    return world;
+}
+
 int main()
 {
-    const int image_width = 500;
-    const int image_height = 250;
+    const int image_width = 800;
+    const int image_height = 400;
     const int samples_per_pixel = 100;
     const int max_depth = 50;
     const auto aspect_ratio = static_cast<double>(image_width) / image_height;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    hittable_list world;
-    world.add(std::make_shared<sphere>(
-        vec3(0, 0, -1), 0.5,
-        std::make_shared<lambertian>(vec3(0.1, 0.2, 0.5))));
-    world.add(std::make_shared<sphere>(
-        vec3(0, -100.5, -1), 100,
-        std::make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
-    world.add(std::make_shared<sphere>(
-        vec3(1, 0, -1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2))));
-    world.add(std::make_shared<sphere>(vec3(-1, 0, -1), 0.5,
-                                       std::make_shared<dielectric>(1.5)));
-    world.add(std::make_shared<sphere>(vec3(-1, 0, -1), -0.45,
-                                       std::make_shared<dielectric>(1.5)));
+    const auto world = random_scene();
 
-    const vec3 lookfrom{3, 3, 2};
-    const vec3 lookat{0, 0, -1};
+    const vec3 lookfrom{13, 2, 3};
+    const vec3 lookat{0, 0, 0};
     const vec3 vup{0, 1, 0};
-    const auto dist_to_focus = (lookfrom - lookat).length();
-    const auto aperture = 2.0;
+    const auto dist_to_focus = 10.0;
+    const auto aperture = 0.1;
 
     const camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture,
                      dist_to_focus);
--- src/metal.hpp
+++ src/metal.hpp
@@ -16,7 +16,7 @@
 class metal final : public material
 {
  public:
-    metal(const vec3& a) : albedo(a)
+    metal(const vec3& a, double f) : albedo(a), fuzz(f < 1 ? f : 1)
     {
         // Do nothing
     }
@@ -26,13 +26,14 @@ class metal final : public material
     {
         const vec3 reflected =
             reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray{rec.p, reflected + fuzz * random_in_unit_sphere()};
         attenuation = albedo;
 
         return dot(scattered.direction(), rec.normal) > 0;
     }
 
     vec3 albedo;
+    double fuzz;
 };
 
 #endif
